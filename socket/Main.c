776bb43d879756f66b614229e6a102acd36dc72c
gaowanlu <2209120827@qq.com>
2021-03-22 15:56:53 +0800

    读取http响应头

--- socket/Main.c
+++ socket/Main.c
@@ -6,6 +6,26 @@
 #include<netinet/in.h>
 #include<string.h>
 #include<errno.h>
+
+void processClient_Socket(int *clientSocket){
+   char buffer[512]={0};
+   size_t len=0;
+   printf("请求头\n");
+   while(0!=(len=read(*clientSocket,buffer,511))){
+       buffer[len]='\0';
+       printf("%s",buffer);
+       if(len-5>=0){//短连接没有开启keepalive,则可以判断是否关闭连接判断请求头是否结束,时长连接一直到\r\n\r\n说明请求头结束
+           if(buffer[len-1]=='\n'&&buffer[len-2]=='\r'&&buffer[len-3]=='\n'&&buffer[len-4]=='\r'){
+               break;
+           }
+       }else{
+           break;
+       }
+   }
+   printf("\n");
+   //shutdown(*clientSocket,SHUT_RD);
+}
+
 int main(void){
     //输入传输文件地址
     char *filename="./data.txt\0";
@@ -38,24 +58,29 @@ int main(void){
     //创建客户端信息结构体
     struct sockaddr_in client_address;
     socklen_t client_address_size=sizeof(client_address);
+    //单线程接受请求
     while(1){
-    //进行接受请求,获取客户端套接字client_socket
-    int client_socket=accept(server_socket,(struct sockaddr*)&client_address,&client_address_size);
-    printf("接受请求\n");
-    //定义缓冲区
-    char buffer[512]={0};
-    size_t count;
-    while((count=fread(buffer,1,512,file))>0){
-        write(client_socket,buffer,count);
-    }
-    //关闭向客户端读写数据通道
-    shutdown(client_socket,SHUT_WR);
-    printf("受理成功\n");
-    //关闭文件
-    fclose(file);
-    //关闭服务端与客户端的套接字
-    close(client_socket);
-    FILE* file=fopen(filename,"r");
+        //进行接受请求,获取客户端套接字client_socket
+        int client_socket=accept(server_socket,(struct sockaddr*)&client_address,&client_address_size);
+        //从客户端套接字得到http请求头
+        printf("接收数据");
+        processClient_Socket(&client_socket);
+        printf("发送数据\n");
+        //定义缓冲区
+        char buffer[512]={0};
+        size_t count;
+        while((count=fread(buffer,1,512,file))>0){
+            write(client_socket,buffer,count);
+        }
+        //关闭向客户端读写数据通道
+        shutdown(client_socket,SHUT_WR);
+        printf("受理成功\n");
+        //关闭文件
+        fclose(file);
+        file=NULL;
+        //关闭服务端与客户端的套接字
+        close(client_socket);
+        file=fopen(filename,"r");
     }
     close(server_socket);
     printf("Over!");
